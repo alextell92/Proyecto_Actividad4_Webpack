ed82720000e4788d8823eafcb93f5c8b54bcde26
alex <83183043+alextell92@users.noreply.github.com>
2025-04-23 20:20:54 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -60,4 +60,3 @@ module.exports = {
 
 
 
-Proyecto final del modulo 5, cree una aplicacion para agregar filtros a una imagen cargada por el usuario final, utilice activos, imagenes, css, scss, yaml,jason5, biblioteca externa.
